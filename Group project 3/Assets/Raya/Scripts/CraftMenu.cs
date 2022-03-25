d25b7067caed07b2fbc78b723df3af9e0e5b4818
rpeyn <74733523+rpeyn@users.noreply.github.com>
2022-03-25 14:14:42 +0100

    Fog of War fixed

--- Group project 3/Assets/Raya/Scripts/CraftMenu.cs
+++ Group project 3/Assets/Raya/Scripts/CraftMenu.cs
@@ -5,38 +5,38 @@ using UnityEngine.UI;
 
 public class CraftMenu : MonoBehaviour
 {
-    //public GameObject Cam;
+    public GameObject Cam;
     public GameObject craftingMenu;
-    private GameObject player;
+   // private GameObject player;
 
 
     private void Start()
     {
-        player = GameObject.Find("Player");
+       // player = GameObject.Find("Player");
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.Confined;
+            //Cursor.visible = true;
+            //Cursor.lockState = CursorLockMode.Confined;
             //Time.timeScale = 0f;
-            player.SetActive(false);
-            craftingMenu.SetActive(true);
-            //OpenCraftMenu();
+            //player.SetActive(false);
+            //craftingMenu.SetActive(true);
+            OpenCraftMenu();
         }
     }
 
-    //public void OpenCraftMenu()
-    //{
-    //    if (craftingMenu != null)
-    //    {
-    //        //Cursor.visible = true;
-    //        //Cursor.lockState = CursorLockMode.None;
-    //        //Time.timeScale = 0f;
-    //        //Cam.SetActive(false);
-    //        craftingMenu.SetActive(true);
-    //    }
-    //}
+    public void OpenCraftMenu()
+    {
+        if (craftingMenu != null)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            //Time.timeScale = 0f;
+            Cam.SetActive(false);
+            craftingMenu.SetActive(true);
+        }
+    }
 }
