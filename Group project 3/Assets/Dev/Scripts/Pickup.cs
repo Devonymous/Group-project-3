7c92b7e66369ef83d310fe88972da523d74353ce
Devonymous <54335923+Devonymous@users.noreply.github.com>
2022-03-28 12:42:40 +0200

    pickup herbs

---
+++ Group project 3/Assets/Dev/Scripts/Pickup.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public bool Pickup_able = false;
+    public GameObject Herb;
+    public GameObject UI_e;
+    void Update()
+    {
+        _Pickup();
+    }
+
+    public void _Pickup()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Pickup_able == true) 
+            {
+                Destroy(Herb);
+                Herb = null;
+                UI_e.SetActive(false);
+                Pickup_able = false;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider Other)
+    {
+        if (Other.gameObject.tag == "Herb & Gem")
+        {
+            Pickup_able = true;
+            UI_e.SetActive(true);
+            Herb = Other.gameObject; 
+        }
+    }
+    void OnTriggerExit(Collider Other)
+    {
+        if (Other.gameObject.tag == "Herb & Gem")
+        {
+            UI_e.SetActive(false);
+            Pickup_able = false;
+        }
+    }
+
+}
