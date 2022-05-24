814ff8591556656e6345cb7ae06cf7bf8723e676
rpeyn <74733523+rpeyn@users.noreply.github.com>
2022-05-24 12:45:08 +0200

    Fixed Map
    No button opening the map yet

---
+++ Group project 3/Assets/Raya/Scripts/MiniMap.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMap : MonoBehaviour
+{
+    public Button button;
+
+    void Start()
+    {
+        //Button btn = button.GetComponent<Button>();
+        //button.onClick.AddListener(RunThisTask);
+    }
+
+    void RunThisTask()
+    {
+        //do something
+    }
+}
